d29547e13123cf115b9232340e10cb64b98c0306
derryman <derrymahon@icloud.com>
2024-09-28 11:01:02 +0100

    Add program to compute average

---
+++ average.py
@@ -0,0 +1,5 @@
+numbers = input("Enter numbers seperated b y space: ").split()
+numbers = [float(num) for num in numbers]
+
+average = sum(numbers) / len(numbers)
+print(f"THe average is: {average}")
