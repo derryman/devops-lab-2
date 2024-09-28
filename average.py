fc12c5eef6d75ea148f0b0ff8e2e2865053f82e9
Niall <niall1707@gmail.com>
2024-09-28 22:41:09 +0100

    changing advrage file so it displayes the biggest and smalles numbers

--- average.py
+++ average.py
@@ -3,3 +3,10 @@ numbers = [float(num) for num in numbers]
 
 average = sum(numbers) / len(numbers)
 print(f"THe average is: {average}")
+
+
+numbers = input("Enter numbers seperated b y space: ").split()
+numbers = [float(num) for num in numbers]
+
+average = sum(numbers) / len(numbers)
+print(f"THe average is: {average}")
